1a38de436551cdfb89c1f44df34b7697ee3d5fcc
Thana <46461678+ThanaOrchard@users.noreply.github.com>
2019-01-20 14:03:52 -0800

    Add js file via upload.

---
+++ card-game.js
@@ -0,0 +1,40 @@
+// JavaScript source code
+// gonna start with pseudo-code first
+
+//
+
+// VARS
+
+// var cardStats// contains variables each card will have // name, image, audio, type, rank?
+
+// var card // object? or whatever its called, one for each card instance
+
+// var deck // array // contains all card objects
+
+//
+
+// FUNCTIONS
+
+// function start game // trigger deck shuffle and draw
+
+// function shuffle deck // separate from draw in case i want to shuffle remaining cards at some point?
+
+// function draw // draws 1-3 cards
+
+// animation for each card drawn? show card before placing on "table"?? maybe future polish
+
+// function hover/click // show card details on hover/click?
+
+// function combineCards // DO THIS LATER / FUTURE FEATURE
+
+// function finalizeHand // creates a generated story/reading or soemthing based on your hand
+
+// function restart // clears hand, shuffles deck, draws new hand
+
+//
+
+// TECHNICAL FUNCTIONS
+
+// SOUND / AUDIO
+
+// SOCIAL MEDIA SHARING?
